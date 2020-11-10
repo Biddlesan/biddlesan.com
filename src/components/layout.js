95db61297d46677b9c597979c293a750795a6c76
noletorious <naatorres@gmail.com>
2020-11-09 18:25:31 -0800

    kyles website

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import Container from "react-bootstrap/Container";
+import "./layout.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Container>{children}</Container>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
